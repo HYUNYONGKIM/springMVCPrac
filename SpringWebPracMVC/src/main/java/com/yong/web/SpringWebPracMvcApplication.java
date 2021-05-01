b15c503b8ed3fb7e5ce8878ffb7b6b68d3e68b0a
DESKTOP-4RR8UEB\siggr <siggraph.k@gmail.com>
2021-05-01 18:39:13 +0900

    Spring Starter Project 시작

---
+++ SpringWebPracMVC/src/main/java/com/yong/web/SpringWebPracMvcApplication.java
@@ -0,0 +1,13 @@
+package com.yong.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebPracMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebPracMvcApplication.class, args);
+	}
+
+}
---
+++ SpringWebPracMVC/src/test/java/com/yong/web/SpringWebPracMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yong.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebPracMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
