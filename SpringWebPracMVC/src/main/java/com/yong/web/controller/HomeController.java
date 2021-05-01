b4bcbae6cfac61ae52555da1f0f5e6af2ad2ef91
DESKTOP-4RR8UEB\siggr <siggraph.k@gmail.com>
2021-05-01 22:26:03 +0900

    홈 컨트롤러 수정

--- SpringWebPracMVC/src/main/java/com/yong/web/controller/HomeController.java
+++ SpringWebPracMVC/src/main/java/com/yong/web/controller/HomeController.java
@@ -4,12 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 	
-	@RequestMapping("index")
+	@RequestMapping("/index")
 	public String index() {
 		
-		return "root.index";
+		return "/WEB-INF/view/index.jsp";
 	}
 }
